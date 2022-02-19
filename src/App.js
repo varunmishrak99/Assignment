57b1040c716e94006a49db38bedc59c2c5ab844b
shmiitg <shubhamank002@gmail.com>
2022-02-19 16:19:31 +0530

    tab indent changed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import UserContextProvider from './context/UserContext';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import UserContextProvider from "./context/UserContext";
 
 function App() {
-  return (
-    <div className="App">
-      <UserContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-          </Routes>
-        </Router>
-      </UserContextProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <UserContextProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                    </Routes>
+                </Router>
+            </UserContextProvider>
+        </div>
+    );
 }
 
 export default App;
